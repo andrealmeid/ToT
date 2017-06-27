33561cdf31630d59192bdcb1bcc4108ed5b2a5aa
Italo Nicola <italo.niccola@gmail.com>
2017-06-27 13:53:33 +0000

    Remove test3 and improve clone.sh

--- cloner.sh
+++ cloner.sh
@@ -1,5 +1,13 @@
+output="Already up-to-date."
+
 while [ true ]
 do
-    git pull
+    t=$(git pull)
+    if [ t == output ]
+    then
+        killall node
+        node server.js
+    fi
+
     sleep 10
 done
